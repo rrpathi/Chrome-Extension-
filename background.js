ca681461a0f151de89570d44845a5bc9fd325131
Ragupathi <ragupathi@revmakx.com>
2019-01-17 11:07:59 +0530

    chrome Extension backend Works

--- background.js
+++ background.js
@@ -1,33 +1,33 @@
-var contextItem = {
-"id":"ragu",
-"title" :"Save To BookMark",
-"contexts":["all"]
-};
-var localStorageUrl = [];
-chrome.contextMenus.create(contextItem);
-chrome.contextMenus.onClicked.addListener(function(){
-chrome.storage.local.get(function(addressBarUrl) {
-if(addressBarUrl.url == undefined){
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-var tabURL = tabs[0].url;
-localStorageUrl.push(tabURL);
-chrome.storage.local.set({'url': localStorageUrl}, function() {
-console.log('first' + localStorageUrl);
-});
-});
-}else{
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-var tabURL = tabs[0].url;
-localStorageUrl.push(tabURL);
-chrome.storage.local.set({'url': localStorageUrl}, function(e) {
-});
-});
-}
-console.log(addressBarUrl.url);
-});
+	var contextItem = {
+		"id":"ragu",
+		"title" :"Save To BookMark",
+		"contexts":["all"]
+	};
+	var localStorageUrl = [];
+	chrome.contextMenus.create(contextItem);
+	chrome.contextMenus.onClicked.addListener(function(){
+		getUrl();
+	});
 
-});
-// });
+	function getUrl(){
+		console.log("inside function");
+		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+		var tabURL = tabs[0].url;
+		if(tabURL.indexOf('http') >= 0){
+			chrome.storage.local.get(function(addressBarUrl){
+				if(addressBarUrl.url == undefined){
+					localStorageUrl.push(tabURL);
+					console.log('if');
+				}else{
+					localStorageUrl = addressBarUrl.url;
+					localStorageUrl.push(tabURL);
+					console.log(localStorageUrl);
+					console.log('else');
+				}
+			});
+		}else{
+			console.log('Its Not A Correct Url');
+		}
 
-
-
+		});
+	}
