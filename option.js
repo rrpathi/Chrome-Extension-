1410f8dfb042550c827ffae7ca59746ad2b1df63
Ragupathi\ragu <ragu3610@gmail.com>
2019-01-18 02:44:29 +0530

    delete storage finished

--- option.js
+++ option.js
@@ -8,6 +8,11 @@ document.addEventListener('DOMContentLoaded', function () {
   		 tag.setAttribute('target','_blank');
   		 tag.appendChild(text);
   		 id.appendChild(tag);
+  		 var btn = document.createElement("button");
+  		 btn.setAttribute('id',value);
+  		 var t = document.createTextNode("Delete");
+  		btn.appendChild(t);
+  		 id.appendChild(btn);
   		 var newElem = document.createElement("br");
   		 id.appendChild(newElem);
 	  	// $('#url_list').append(`<a href="${element}">${element}</a><br />`);
@@ -22,12 +27,16 @@ if (document.readyState === "complete") {  // Loading hasn't finished yet
 		// console.log(myInput.value);
 		 var div = document.getElementById("url_list");
 		 var a_list = div.getElementsByTagName("a");
+		 var a_button = div.getElementsByTagName("button");
 		 for(var i = 0; i<a_list.length;i++){
 		 	   var content  = a_list[i].textContent || a_list[i].innerText;
 		 	   if(content.indexOf(search_input) > -1){
 		 	   		a_list[i].style.display = "";
+		 	   		a_button[i].style.display = '';
 		 	   }else {
 		 	   		a_list[i].style.display = "none";
+		 	   		a_button[i].style.display = "none";
+
 		 	   		
 		 	   }
 		 }
@@ -39,3 +48,12 @@ if (document.readyState === "complete") {  // Loading hasn't finished yet
 
 }
 
+document.querySelector('body').addEventListener('click', function(event) {
+	var url_id = event.target.id;
+	chrome.storage.local.get(function(addressBarUrl) {
+		addressBarUrl.url.splice(url_id,1);
+		chrome.storage.local.set({'url': addressBarUrl.url}, function() {
+							// console.log(localStorageUrl);
+						});	
+	});
+});
