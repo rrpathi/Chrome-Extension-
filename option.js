034999a544f8bf27d455ae4482458414ca4d2cb1
Ragupathi <ragupathi@revmakx.com>
2019-01-17 19:04:14 +0530

    option page

--- option.js
+++ option.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-// 	$("#heading").click(function(){
-// 	console.log('Hello World');
-// })
   chrome.storage.local.get(function(addressBarUrl) {
-  	addressBarUrl.url.forEach(function(element){
-	  	$('#url_list').append(`${element}<br />`);
-
+  	addressBarUrl.url.forEach(function(element,value){
+	  	$('#url_list').append(`<a href="${element}">${element}</a><br />`);
   	});
-
 	});
 });
 
