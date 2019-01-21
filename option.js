ee06801a2ea88a48e18aa74eef7cee0d3d706978
Ragupathi\ragu <ragu3610@gmail.com>
2019-01-22 02:59:22 +0530

    Api to Fetch data

--- option.js
+++ option.js
@@ -1,30 +1,44 @@
 $(document).ready(function(){
-	chrome.storage.local.get(function(addressBarUrl){
-		addressBarUrl.url.forEach(function(value,key){
-	  	$('#myTable').append(`<tr class='singleUrl'><td><a target="_blank" href="${value['url']}">${value['title']}</a></td><td><button class="btn" value="${value['uniqueId']}">Delete</button></td></tr>`);
+	chrome.storage.local.get('localStorageData', function(data) {
+		data.localStorageData.forEach(function(value,key){
+	  	$('#myTable').append(`<tr class='singleUrl'>
+	  		<td style ="width:80%">
+		  		<a target="_blank" href="${value['site_url']}">${value['site_url']}</a>
+		  		<div  style="margin-left:15px;">
+				  		<p style="color:#0b4c8c;font-size:20px;font-weight: 700;">${value['url_title']}</p>
+				  		<p>${value['description']}</p >
+			  			<img src="${value['img_url']}" height="60px" width="60px">
+		  		</div>
+	  		</td>
+	  		<td ><button class="btn" value="${value['uniqueId']}">Delete</button></td></tr>`);
 		});
 	});
+	// chrome.storage.local.get(function(addressBarUrl){
+	// 	addressBarUrl.url.forEach(function(value,key){
+	//   	$('#myTable').append(`<tr class='singleUrl'><td><a target="_blank" href="${value['url']}">${value['title']}</a></td><td><button class="btn" value="${value['uniqueId']}">Delete</button></td></tr>`);
+	// 	});
+	// });
 	$("#myInput").on('keyup',function(){
 		var inputValue = $(this).val().toLowerCase();
 		$("#myTable tr").filter(function() {
       	$(this).toggle($(this).text().toLowerCase().indexOf(inputValue) > -1)
    		 });
 	});
-	$("body").on('click','.btn',function(){
-		var delete_id = this.value;
-		$(this).closest('.singleUrl').remove();
-		console.log(this.value);
-		chrome.storage.local.get(function(addressBarUrl) {
-		var delete_key =  addressBarUrl.url.findIndex(function(url){
-		 	return url.uniqueId == delete_id;
+	// $("body").on('click','.btn',function(){
+	// 	var delete_id = this.value;
+	// 	$(this).closest('.singleUrl').remove();
+	// 	console.log(this.value);
+	// 	chrome.storage.local.get(function(addressBarUrl) {
+	// 	var delete_key =  addressBarUrl.url.findIndex(function(url){
+	// 	 	return url.uniqueId == delete_id;
 
-		 })
-			addressBarUrl.url.splice(delete_key,1);
-			chrome.storage.local.set({'url': addressBarUrl.url}, function() {
-				console.log('deleted ');
-			});
-		});
-	});
+	// 	 })
+	// 		addressBarUrl.url.splice(delete_key,1);
+	// 		chrome.storage.local.set({'url': addressBarUrl.url}, function() {
+	// 			console.log('deleted ');
+	// 		});
+	// 	});
+	// });
 
 });
 
